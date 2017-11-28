d1663d268a5bfc0a2d36241092beb18f88d7a679
m13oas <aozertsov@gmail.com>
2017-11-28 10:46:33 +0300

    rewrite json

--- src/main/java/uk/dsx/Starter.java
+++ src/main/java/uk/dsx/Starter.java
@@ -1,7 +1,6 @@
 package uk.dsx;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import uk.dsx.node.ethereum.EthereumNode;
 
 import java.io.IOException;
 
@@ -10,14 +9,14 @@ import java.io.IOException;
  */
 public class Starter {
     public static void main(String[] args) throws IOException {
-        String json = "{ \"hostIp\" : \"someIp\",\n" +
+        String json = "{\n" +
                 "\"sharedFiles\": [\"pathForJson/Genesis.json\"],\n" +
-                "\"root\" : {\"user\": \"awsUser\", \"ip\": \"awsIp\", \"dockerName\": \"root_node\", \"keyPath\": \"path\"},\n" +
-                "\"common\": [{\"user\": \"awsUser\", \"ip\": \"awsIp\", \"dockerName\": \"common_node\", \"keyPath\": \"path\"}]}";
+                "\"commonDriverFiles\": [\"pathForJson/Genesis.json\"],\n" +
+                "\"rootDriverFiles\": [\"pathForJson/Genesis.json\"],\n" +
+                "\"machines\": [{\"user\": \"awsUser\", \"ip\": \"awsIp\",\"keyPath\": \"/pathForkey\", \"nodes\": [{\"dockerName\": \"common\", \"type\": \"eth\"}] }]}";
         final ObjectMapper mapper = new ObjectMapper();
 
         final Data readValue = mapper.readValue(json, Data.class);
         System.out.println(readValue);
-        EthereumNode root_node = readValue.getRoot();
     }
 }
