634bba33c94fe9027c99afbcab417f2cd93f6235
m13oas <aozertsov@gmail.com>
2018-03-25 20:52:05 +0300

    add methods for ethereum API

--- api/src/main/java/uk/dsx/accord/chain/ethereum/EthereumChain.java
+++ api/src/main/java/uk/dsx/accord/chain/ethereum/EthereumChain.java
@@ -49,4 +49,42 @@ public class EthereumChain extends BasicChain<EthereumPeerInfo,EthereumBlock,Eth
         String result = HttpHelper.<String, List>post(address, "eth_getTransactionCount", asList(address, "pending"));
         return Integer.parseInt(result.substring(2));
     }
+
+    public EthereumBlock getBlock(long blockNumber) throws UnirestException {
+        return HttpHelper.<EthereumBlock, List>post(address, "eth_getBlockByNumber", asList("0x" + Long.toHexString(blockNumber)));
+    }
+
+    public long getLastLastBlockNumber() throws UnirestException {
+        String result = HttpHelper.<String, List>post(address, "eth_blockNumber", EMPTY_PARAMETERS);
+        return Long.parseLong(result.substring(2), 16);
+    }
+
+    public boolean isNetListening() throws UnirestException {
+        return HttpHelper.<Boolean, List>post(address, "net_listening", EMPTY_PARAMETERS);
+    }
+
+    public int getProtocolVersion() throws UnirestException {
+        return HttpHelper.<Integer, List>post(address, "net_listening", EMPTY_PARAMETERS);
+    }
+
+    public String getCoinbase() throws UnirestException {
+        return HttpHelper.<String, List>post(address, "eth_coinbase", EMPTY_PARAMETERS);
+    }
+
+    public boolean isMining() throws UnirestException {
+        return HttpHelper.<Boolean, List>post(address, "eth_mining", EMPTY_PARAMETERS);
+    }
+
+    public int getHashRate() throws UnirestException {
+        return HttpHelper.<Integer, List>post(address, "eth_hashrate", EMPTY_PARAMETERS);
+    }
+
+    public long getGasPrice() throws UnirestException {
+        String result = HttpHelper.<String, List>post(address, "eth_gasPrice", EMPTY_PARAMETERS);
+        return Long.parseLong(result.substring(2), 16);
+    }
+
+    public List<String> getAccounts() throws UnirestException {
+        return HttpHelper.<List<String>, List>post(address, "eth_accounts", EMPTY_PARAMETERS);
+    }
 }
